0d2ddf6b3893c4b4f286746d1fa4c5f2f052ba16
KliqxTV <j.laquai@gmx.de>
2022-11-08 21:47:50 +0100

    Add project files.

---
+++ LaquaiLib/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace LaquaiLib
+{
+    public static class Constants
+    {
+        static IEnumerable<int> Numbers => LaquaiLib.Range(0, 9).Cast<int>();
+        static IEnumerable<char> LettersLowercase => LaquaiLib.Range(65, 90).Cast<char>();
+        static IEnumerable<char> LettersUppercase => LaquaiLib.Range(97, 122).Cast<char>();
+
+
+    }
+}
---
+++ LaquaiLib/Extensions.cs
@@ -0,0 +1,21 @@
+﻿namespace LaquaiLib
+{
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<TResult> Cast<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> caster)
+        {
+            foreach (TSource item in source)
+            {
+                yield return caster(item);
+            }
+        }
+        
+        public static IEnumerable<TResult> Cast<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> caster, Func<TSource, bool> predicate)
+        {
+            foreach (TSource item in source.Where(predicate))
+            {
+                yield return caster(item);
+            }
+        }
+    }
+}
