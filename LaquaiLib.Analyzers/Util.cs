bdcf2bfc9039b16638d6e6af1e7d2636f72550f5
KliqxTV <j.laquai@gmx.de>
2023-11-16 22:02:38 +0100

    g

---
+++ LaquaiLib.Analyzers/Util.cs
@@ -0,0 +1,18 @@
+﻿namespace LaquaiLib.Analyzers;
+
+internal static class Util
+{
+    /// <summary>
+    /// Generates an analyzer ID from the specified type.
+    /// </summary>
+    /// <param name="type">The type.</param>
+    /// <returns>A string representing the analyzer ID.</returns>
+    internal static string GetAnalyzerId(Type type) => $"{type.Namespace}.{type.Name}";
+
+    /// <summary>
+    /// Generates an analyzer category from the specified type.
+    /// </summary>
+    /// <param name="type">The type.</param>
+    /// <returns>A string representing the analyzer category.</returns>
+    internal static string GetAnalyzerCategory(Type type) => type.Namespace ?? typeof(Util).Namespace;
+}
