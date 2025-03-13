fcbf418895e492b19bb45f61c12e10d5da6615f9
Joshua Laquai <laquai.joshua@becksche.de>
2025-03-13 14:58:31 +0100

    Fix

--- LaquaiLib/Extensions/FileSystemModelExtensions.cs
+++ LaquaiLib/Extensions/FileSystemModelExtensions.cs
@@ -8,29 +8,25 @@ namespace LaquaiLib.Extensions;
 public static partial class FileSystemModelExtensions
 {
     /// <summary>
-    /// Creates a new <see cref="DirectoryInfo"/> instance for the subdirectory identified by <paramref name="name"/> if it exists, otherwise returns <see langword="null"/>.
+    /// Creates a new <see cref="DirectoryInfo"/> instance for the subdirectory identified by <paramref name="name"/>.
     /// </summary>
     /// <param name="di">The <see cref="DirectoryInfo"/> instance to use as the current directory.</param>
     /// <param name="name">A relative directory path to search for under the directory represented by <paramref name="di"/>.</param>
-    /// <returns>A <see cref="DirectoryInfo"/> instance for the subdirectory identified by <paramref name="name"/> if it exists, otherwise <see langword="null"/>.</returns>
+    /// <returns>A <see cref="DirectoryInfo"/> instance for the subdirectory identified by <paramref name="name"/>.</returns>
     public static DirectoryInfo Directory(this DirectoryInfo di, string name)
     {
         ArgumentNullException.ThrowIfNull(di);
-        if (di.Exists)
-        {
-            return new DirectoryInfo(Path.Combine(di.FullName, name));
-        }
-        return null;
+        return new DirectoryInfo(Path.Combine(di.FullName, name));
     }
     /// <summary>
-    /// Creates a new <see cref="DirectoryInfo"/> instance for the subdirectory identified by a path consisting of subdirectory <paramref name="names"/> if it exists, otherwise returns <see langword="null"/>.
+    /// Creates a new <see cref="DirectoryInfo"/> instance for the subdirectory identified by a path consisting of subdirectory <paramref name="names"/>.
     /// </summary>
     /// <param name="di">The <see cref="DirectoryInfo"/> instance to use as the current directory.</param>
     /// <param name="names">Any number of directory names to join and search for under the directory represented by <paramref name="di"/>.</param>
-    /// <returns>A <see cref="DirectoryInfo"/> instance for the subdirectory identified by a path consisting of subdirectory <paramref name="names"/> if it exists, otherwise <see langword="null"/>.</returns>
+    /// <returns>A <see cref="DirectoryInfo"/> instance for the subdirectory identified by a path consisting of subdirectory <paramref name="names"/>.</returns>
     public static DirectoryInfo Directory(this DirectoryInfo di, params ReadOnlySpan<string> names) => di.Directory(Path.Combine(names));
     /// <summary>
-    /// Creates a new <see cref="DirectoryInfo"/> instance for the subdirectory identified by <paramref name="name"/>, creating the entire directory structure it does not exist.
+    /// Creates a new <see cref="DirectoryInfo"/> instance for the subdirectory identified by <paramref name="name"/>, creating the entire directory structure if it does not exist.
     /// </summary>
     /// <param name="di">The <see cref="DirectoryInfo"/> instance to use as the current directory.</param>
     /// <param name="name">A relative directory path to search for under the directory represented by <paramref name="di"/>.</param>
@@ -46,7 +42,7 @@ public static partial class FileSystemModelExtensions
         return directory;
     }
     /// <summary>
-    /// Creates a new <see cref="DirectoryInfo"/> instance for the subdirectory identified by a path consisting of subdirectory <paramref name="names"/>, creating the entire directory structure it does not exist.
+    /// Creates a new <see cref="DirectoryInfo"/> instance for the subdirectory identified by a path consisting of subdirectory <paramref name="names"/>, creating the entire directory structure if it does not exist.
     /// </summary>
     /// <param name="di">The <see cref="DirectoryInfo"/> instance to use as the current directory.</param>
     /// <param name="names">Any number of directory names to join and search for under the directory represented by <paramref name="di"/>.</param>
@@ -54,27 +50,23 @@ public static partial class FileSystemModelExtensions
     public static DirectoryInfo MakeDirectory(this DirectoryInfo di, params ReadOnlySpan<string> names) => di.MakeDirectory(Path.Combine(names));
 
     /// <summary>
-    /// Creates a <see cref="FileInfo"/> instance for a file identified by <paramref name="name"/>. If <paramref name="di"/> does not exist, <see langword="null"/> is returned.
+    /// Creates a <see cref="FileInfo"/> instance for a file identified by <paramref name="name"/>.
     /// </summary>
     /// <param name="di">The <see cref="DirectoryInfo"/> instance to use as the current directory.</param>
     /// <param name="name">A relative file path to search for under the directory represented by <paramref name="di"/>.</param>
-    /// <returns>A <see cref="FileInfo"/> instance for a file identified by <paramref name="name"/> or <see langword="null"/> if <paramref name="di"/> does not exist.</returns>
+    /// <returns>A <see cref="FileInfo"/> instance for a file identified by <paramref name="name"/>.</returns>
     public static FileInfo File(this DirectoryInfo di, string name)
     {
         ArgumentNullException.ThrowIfNull(di);
-        if (di.Exists)
-        {
-            return new FileInfo(Path.Combine(di.FullName, name));
-        }
-        return null;
+        return new FileInfo(Path.Combine(di.FullName, name));
     }
     /// <summary>
-    /// Creates a <see cref="FileInfo"/> instance for a file identified by <paramref name="names"/>. If <paramref name="di"/> does not exist, <see langword="null"/> is returned.
+    /// Creates a <see cref="FileInfo"/> instance for a file identified by <paramref name="names"/>.
     /// </summary>
     /// <param name="di">The <see cref="DirectoryInfo"/> instance to use as the current directory.</param>
     /// <param name="names">Any number of directory names to join and search for under the directory represented by <paramref name="di"/>.</param>
-    /// <returns>A <see cref="FileInfo"/> instance for a file identified by <paramref name="names"/> or <see langword="null"/> if <paramref name="di"/> does not exist.</returns>
-    public static FileInfo File(this DirectoryInfo di, params ReadOnlySpan<string> names) => di.GetFile(Path.Combine(names));
+    /// <returns>A <see cref="FileInfo"/> instance for a file identified by <paramref name="names"/>.</returns>
+    public static FileInfo File(this DirectoryInfo di, params ReadOnlySpan<string> names) => di.File(Path.Combine(names));
 
     /// <summary>
     /// Determines whether the directory represented by <paramref name="di"/> is a base of the directory represented by <paramref name="comp"/>.
