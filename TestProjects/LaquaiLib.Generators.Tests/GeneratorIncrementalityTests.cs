ca19b0e2188c96c09eaee9ce61ba5c8ae50a95cd
jolaquai <j.laquai@gmx.de>
2026-08-14 18:27:37 +0200

    add incrementality tests for InlineArrayExtensionsGenerator (red)

--- TestProjects/LaquaiLib.Generators.Tests/GeneratorIncrementalityTests.cs
+++ TestProjects/LaquaiLib.Generators.Tests/GeneratorIncrementalityTests.cs
@@ -1,5 +1,7 @@
 namespace LaquaiLib.Generators.Tests;
 
+using LaquaiLib.Generators.SourceGeneratedExtensions;
+
 public static class GeneratorIncrementalityTests
 {
     private const string UnrelatedSource = "class TotallyUnrelated { void M() { } }";
@@ -12,6 +14,16 @@ public static class GeneratorIncrementalityTests
     private const string ProxySourceEdited = """
         [FullAccessProxy(typeof(global::System.Text.StringBuilder))]
         public partial class MsProxy { }
+        """;
+
+    private const string InlineArraySource = """
+        [global::System.Runtime.CompilerServices.InlineArray(4)]
+        public struct Buf4 { private int _e0; }
+        """;
+
+    private const string InlineArraySourceEdited = """
+        [global::System.Runtime.CompilerServices.InlineArray(8)]
+        public struct Buf4 { private int _e0; }
         """;
 
     private static SyntaxTree Parse(string source, string path)
@@ -71,5 +83,55 @@ public static class GeneratorIncrementalityTests
         [Fact]
         public void InitializeHasNoCapturingLambdas()
             => CapturingLambdaAssertions.AssertNoCapturingLambdas<FullAccessProxyGenerator>();
+    }
+
+    public class InlineArrayExtensions
+    {
+        // THE MONEY TEST: fails today because ForAttributeWithMetadataNameOn returns the raw
+        // GeneratorAttributeSyntaxContext as the model, so .Collect() can never compare equal.
+        [Fact]
+        public void UnrelatedEditKeepsSourceOutputCached()
+        {
+            var (driver, compilation, _) = GeneratorTestHost.RunTracked(new InlineArrayExtensionsGenerator(), InlineArraySource);
+            var result = GeneratorTestHost.RunAgain(driver, WithUnrelatedFile(compilation));
+            GeneratorTestHost.AssertStepsCached(result, GeneratorTestHost.SourceOutputStepName);
+        }
+
+        [Fact]
+        public void NoOpRerunKeepsSourceOutputCached()
+        {
+            var (driver, compilation, _) = GeneratorTestHost.RunTracked(new InlineArrayExtensionsGenerator(), InlineArraySource);
+            var result = GeneratorTestHost.RunAgain(driver, compilation);
+            GeneratorTestHost.AssertStepsCached(result, GeneratorTestHost.SourceOutputStepName);
+        }
+
+        [Fact]
+        public void EquivalentButFreshlyParsedTreesKeepSourceOutputCached()
+        {
+            var (driver, _, _) = GeneratorTestHost.RunTracked(new InlineArrayExtensionsGenerator(), InlineArraySource);
+            var freshCompilation = GeneratorTestHost.CreateCompilation(InlineArraySource);
+            var result = GeneratorTestHost.RunAgain(driver, freshCompilation);
+            GeneratorTestHost.AssertStepsCached(result, GeneratorTestHost.SourceOutputStepName);
+        }
+
+        [Fact]
+        public void RelevantEditRerunsSourceOutput()
+        {
+            var (driver, _, _) = GeneratorTestHost.RunTracked(new InlineArrayExtensionsGenerator(), InlineArraySource);
+            var editedCompilation = GeneratorTestHost.CreateCompilation(InlineArraySourceEdited);
+            var result = GeneratorTestHost.RunAgain(driver, editedCompilation);
+            GeneratorTestHost.AssertStepsRan(result, GeneratorTestHost.SourceOutputStepName);
+        }
+
+        [Fact]
+        public void ModelHoldsNoRoslynObjects()
+        {
+            var (_, _, result) = GeneratorTestHost.RunTracked(new InlineArrayExtensionsGenerator(), InlineArraySource);
+            ModelPurityAssertions.AssertNoRoslynObjectsInModel(result);
+        }
+
+        [Fact]
+        public void InitializeHasNoCapturingLambdas()
+            => CapturingLambdaAssertions.AssertNoCapturingLambdas<InlineArrayExtensionsGenerator>();
     }
 }
